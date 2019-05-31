e51e5dd33cef7200df49fb4edd88f0b56208ab35
Vishal_Dhaware <dhaware090@gmail.com>
2019-05-31 13:14:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vishal_dhaware/sharedperferences/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vishal_dhaware.sharedperferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
